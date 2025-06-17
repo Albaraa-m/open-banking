5ce7c5b2233a22c6d5f98b8887d1f80f893b3f65
Albaraa Mirdad <albaraa.m@hotmail.com>
2025-06-17 22:19:00 +0300

    feat: setup db

---
+++ backend/database.py
@@ -0,0 +1,27 @@
+from contextlib import contextmanager
+from typing import Generator
+
+from constants import DATABASE_URL
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+Base.metadata.create_all(bind=engine)
+
+
+@contextmanager
+def get_db() -> Generator:
+    db = SessionLocal()
+    try:
+        yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    finally:
+        db.close()
